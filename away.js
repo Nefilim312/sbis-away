5d6a3cd53b8af40e6fe31490378f0e4a6e60c000
da.ivanov <da.ivanov@tensor.ru>
2020-10-02 11:55:43 +0300

    New version

--- away.js
+++ away.js
@@ -1,53 +1,146 @@
-let robot = require("robotjs");
+const os = require('os');
+const fetch = require('node-fetch');
+const robot = require('robotjs');
 
-let cursorPostition = robot.getMousePos().x + robot.getMousePos().y;
-let startHour = process.argv[2] || 9;
-let endHour = process.argv[3] || 18;
+const hostname = os.hostname();
+const INTERVAL = 3;
 
-console.log(`Эмуляция активности с ${startHour} до ${endHour}`);
+class Emulator {
+    constructor({sid, startTime = 9, endTime = 18} = {}) {
+        if (!sid) {
+            console.info('\x1b[7m\x1b[31m', 'Не передан sid ');
+            return process.exit();
+        }
 
-function isWorkingTime() {
-   let time = new Date();
+        this._sid = sid;
+        this._startTime = startTime;
+        this._endTime = endTime;
+        this._cursorPos = robot.getMousePos().x + robot.getMousePos().y;
+        this._noActivityStart = null;
 
-   if (time.getDay() < 6 && time.getDay() > 0) {
-      if (time.getHours() >= startHour && time.getHours() < endHour) {
-         return true;
-      } else {
-         return false;
-      }
-   } else {
-      return false;
-   }
+        console.info(`Эмуляция активности с ${this._startTime} до ${this._endTime} для ${hostname}`);
+    }
+
+    /**
+     * Проверка активности за компьютером
+     */
+    checkActivity = () => {
+        if (!this.isWorkingTime()) return;
+
+        // Нет активности
+        if (this._cursorPos === robot.getMousePos().x + robot.getMousePos().y) {
+            if (!this._noActivityStart) {
+                this._noActivityStart = new Date();
+            }
+            this.emulateActivity();
+            // Есть активность
+        } else {
+            if (this._noActivityStart) {
+                console.info(`Отсутствие активности с ${this._noActivityStart.toLocaleTimeString()} до ${new Date().toLocaleTimeString()}`);
+                this.pushAppActivity(this._noActivityStart, new Date())
+                this._noActivityStart = null;
+            }
+
+            this._cursorPos = robot.getMousePos().x + robot.getMousePos().y;
+        }
+    }
+
+    /**
+     * Эмуляция активности. Шлет запрос для подтверждения, что пользователь за компьютером
+     * @returns {Promise<void>}
+     */
+    async emulateActivity() {
+        const body = JSON.stringify({
+            jsonrpc: '2.0',
+            method: 'Местоположение.Фиксация',
+            params: {'Данные': `{"ВременнаяЗона":"3.00","ДанныеУстройства":"${hostname.toLowerCase()}"}`},
+            protocol: 6,
+            id: 0
+        });
+
+        fetch('https://online.sbis.ru/activity-write/service/', {
+            method: 'POST',
+            body,
+            credentials: 'include',
+            headers: {
+                cookie: `sid=${this._sid}`
+            }
+        })
+    }
+
+    /**
+     * Делаем вид, что во время эмуляции работали в указанном приложении
+     * @param timeStart
+     * @param timeEnd
+     */
+    pushAppActivity(timeStart, timeEnd) {
+        const timeEndFixed = new Date(timeEnd);
+
+        // Урезаем время конца, чтобы не получилось пересечение с настоящим трекингом
+        timeEndFixed.setMinutes(timeEndFixed.getMinutes() - INTERVAL);
+
+        const body = JSON.stringify({
+            jsonrpc: '2.0',
+            method: 'UnproductiveLog.Push',
+            params: {
+                Data: {
+                    'f': 0,
+                    'd': [[0, 'webstorm64', 'index.js - WebStorm', `${this.dateToSQLString(timeStart)}`, `${this.dateToSQLString(timeEndFixed)}`]],
+                    's': [{'n': 'Type', 't': 'Число целое'},
+                        {'n': 'Name', 't': 'Строка'},
+                        {'n': 'Description', 't': 'Строка'},
+                        {'n': 'DateTimeBegin', 't': {'n': 'Дата и время', 'tz': false}},
+                        {'n': 'DateTimeEnd', 't': {'n': 'Дата и время', 'tz': false}}],
+                    '_type': 'recordset'
+                },
+                Parameters: {
+                    'f': 0,
+                    'd': [59],
+                    's': [{'n': 'Version', 't': 'Число целое'}],
+                    '_type': 'record'
+                }
+            },
+            protocol: 6,
+            id: 0
+        });
+
+        fetch('https://online.sbis.ru/activity-write/service/', {
+            method: 'POST',
+            body,
+            credentials: 'include',
+            headers: {
+                cookie: `sid=${this._sid}`
+            }
+        })
+    }
+
+    /**
+     * Проверка времени. Нужно ли эмулировать активности или сейчас нерабочее время
+     * @returns {boolean}
+     */
+    isWorkingTime() {
+        const currentDate = new Date();
+
+        if (currentDate.getDay() < 6 && currentDate.getDay() > 0) {
+            return (currentDate.getHours() >= this._startTime && currentDate.getHours() < this._endTime)
+        } else {
+            return false;
+        }
+    }
+
+    dateToSQLString(date) {
+        return date.toISOString().replace('T', ' ').replace(/\.\w*/g, '');
+    }
+
+    /**
+     * Старт эмулятора
+     * Проверка раз в INTERVAL минут
+     */
+    run() {
+        setInterval(this.checkActivity, 1000 * 60 * INTERVAL);
+    }
 }
 
-function checkCursorPosition() {
-   if (isWorkingTime()) {
-      let currentCursorPosition = robot.getMousePos().x + robot.getMousePos().y;
+const emulator = new Emulator({sid: process.argv[2], startTime: process.argv[3], endTime: process.argv[4]})
 
-      if (currentCursorPosition != cursorPostition) {
-         cursorPostition = currentCursorPosition;
-      } else {
-         emulateActivity();
-         cursorPostition = robot.getMousePos().x + robot.getMousePos().y;
-      }
-   }
-}
-
-function emulateActivity() {
-
-   function getRandomInt(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-   }
-
-   let max = 200;
-   let min = -200;
-   let shift = getRandomInt(min, max);
-
-   robot.keyTap("tab", "alt");
-   robot.moveMouseSmooth(robot.getMousePos().x + shift, robot.getMousePos().y + shift);
-   robot.mouseClick();
-   robot.keyTap("tab", "alt");
-   robot.mouseClick();
-}
-
-var updater = setInterval(checkCursorPosition, 1000 * 60 * 2);
+emulator.run();
